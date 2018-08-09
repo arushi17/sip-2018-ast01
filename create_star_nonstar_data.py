f25b66fa4b4e3a1fb8ce648433cddaeb44e8acd8
Arushi Sahai <arushi@sinha-sahai.com>
2018-08-09 12:06:10 -0700

    Exclude TiO (M class) stars using notes.txt and ppxf.dat files

--- create_star_nonstar_data.py
+++ create_star_nonstar_data.py
@@ -7,6 +7,7 @@ import glob
 import os.path
 import re
 import shutil
+import sys
 
 TARGET_DIR = os.path.expanduser("~") + '/ast01/training_data'
 
@@ -36,8 +37,10 @@ def unique_obj_id(mask_variant, objname):
             'gn1c': 'gn1',
             'gn1d': 'gn1',
             'gn1e_t': 'gn1',
+            'gs0d': 'gs0',
             'gs0d_0': 'gs0',
             'gs1d_0': 'gs1',
+            'gs1d': 'gs1',
     }
 
     true_mask = ''
@@ -51,21 +54,20 @@ def unique_obj_id(mask_variant, objname):
 
     return true_mask + '.' + objname
 
-def copy_1_fits_file(source_data_dir, maskname, slitname, objname, zqual, target_dir, known_zqual):
+def copy_1_fits_file(source_data_dir, maskname, slitname, objname, zqual, target_dir):
     filename = "spec1d." + maskname + "." + '{:0>3}'.format(slitname) + "." + objname + ".fits"
     zqual = int(zqual)
     # Keep track of each object that we have known classification for, even if we are not
     # interested in each class. objname seems to be unique only within a mask.
-    known_zqual[unique_obj_id(maskname, objname)] = zqual
     if zqual == 1 or zqual == 3 or zqual == 4:
         print('zqual ' + str(zqual) + ': ' + source_data_dir + '/' + filename + ' --> ' + target_dir + '/star')
-        shutil.copy(source_data_dir + '/' + filename, target_dir + '/star')
+        #shutil.copy(source_data_dir + '/' + filename, target_dir + '/star')
     if zqual == 0:
         print('zqual ' + str(zqual) + ': ' + source_data_dir + '/' + filename + ' --> ' + target_dir + '/nonstar')
-        shutil.copy(source_data_dir + '/' + filename, target_dir + '/nonstar')
+        #shutil.copy(source_data_dir + '/' + filename, target_dir + '/nonstar')
 
 
-def copy_from_1_directory_zspec_fits(survey_dir, fits_data, target_dir, known_zqual):
+def copy_from_1_directory_zspec_fits(survey_dir, fits_data, target_dir, known_zqual, do_not_copy_map):
     maskname = fits_data.field('MASKNAME')[0]
     print('\n++++++++ Processing mask: {} from zspec.fits'.format(maskname))
     
@@ -79,10 +81,15 @@ def copy_from_1_directory_zspec_fits(survey_dir, fits_data, target_dir, known_zq
     
     source_data_dir = survey_dir + '/' + maskname
     for i in range(len(zqual)):
-        copy_1_fits_file(source_data_dir, maskname, slitname[i], objname[i], zqual[i], target_dir, known_zqual)
+        objid = unique_obj_id(maskname, objname[i])
+        if objid in do_not_copy_map:
+            continue
+        known_zqual[objid] = int(zqual[i])
+        copy_1_fits_file(source_data_dir, maskname, slitname[i], objname[i], zqual[i], target_dir)
 
 
-def copy_from_1_directory_ppxf_path(survey_dir, ppxf_path, target_dir, known_zqual):
+# Also fills do_not_copy_map from the ppxf.dat file
+def copy_from_1_directory_ppxf_path(survey_dir, ppxf_path, target_dir, known_zqual, do_not_copy_map):
     # Extract maskname from filename
     maskname = re.search(r"(.+?)_ppxf.dat", os.path.basename(ppxf_path)).group(1)
     print('\n++++++++ Processing mask: {} from ppxf.dat'.format(maskname))
@@ -103,16 +110,47 @@ def copy_from_1_directory_ppxf_path(survey_dir, ppxf_path, target_dir, known_zqu
         objname.append(split_line_list[0])
         slitname.append(split_line_list[1])
         zqual.append(split_line_list[6])
+
+        if line.find('TiO') > -1 or line.find(' M ') > -1:
+            do_not_copy_map[unique_obj_id(maskname, split_line_list[0])] = 0
+            print('Found a TiO/M star, will exclude from dataset')
     
     print(slitname)
     print(objname)
     print(zqual)
     for i in range(len(zqual)):
-        copy_1_fits_file(source_data_dir, maskname, slitname[i], objname[i], zqual[i], target_dir, known_zqual)
+        objid = unique_obj_id(maskname, objname[i])
+        if objid in do_not_copy_map:
+            continue
+        known_zqual[objid] = int(zqual[i])
+        copy_1_fits_file(source_data_dir, maskname, slitname[i], objname[i], zqual[i], target_dir)
 
+
+def do_not_copy_map_from_txt(survey_dir, do_not_copy_map):
+    # for halo7d only, checks all .txt files for mention of "TiO" or " M "
+    # returns map of TiO/M objects
+    txt_files = glob.glob(survey_dir + '/zspec/*notes.txt')
+    for txt_file in txt_files:
+        print(txt_file)
+
+        with open(txt_file) as f:
+            lines_list = f.readlines()
+        for i in range(len(lines_list)):
+            line = lines_list[i]
+            split_line_list = line.split()
+            if len(split_line_list) < 4:
+                continue
+            objname = split_line_list[1]
+            maskname = os.path.basename(txt_file)[0:os.path.basename(txt_file).find('_')]
+            
+            if line.find('TiO') > -1 or line.find(' M ') > -1:
+                do_not_copy_map[unique_obj_id(maskname, objname)] = 0
+                print('Found a TiO/M star, will exclude from dataset')
+            
 
 # Process HALO7D survey
-def process_halo7d_survey(survey_dir, target_dir, known_zqual):
+def process_halo7d_survey(survey_dir, target_dir, known_zqual, do_not_copy_map):
+    do_not_copy_map_from_txt(survey_dir, do_not_copy_map)
     zspec_fits = glob.glob(survey_dir + '/zspec/zspec*.fits')
     for fits_file in zspec_fits:
         zspec = fits.open(fits_file)
@@ -120,12 +158,12 @@ def process_halo7d_survey(survey_dir, target_dir, known_zqual):
         data = zspec[1].data
     
         print(fits_file)
-        copy_from_1_directory_zspec_fits(survey_dir, data, target_dir, known_zqual)
+        copy_from_1_directory_zspec_fits(survey_dir, data, target_dir, known_zqual, do_not_copy_map)
 
     ppxf_list = glob.glob(survey_dir + '/zspec/*_ppxf.dat')
     for ppxf_file in ppxf_list:
         print(ppxf_file)
-        copy_from_1_directory_ppxf_path(survey_dir, ppxf_file, target_dir, known_zqual)
+        copy_from_1_directory_ppxf_path(survey_dir, ppxf_file, target_dir, known_zqual, do_not_copy_map)
 
     # All halo7d files NOT mentioned in zspec.fits or ppxf.dat are nonstars 
     extra_dirs = [survey_dir + '/GN3_cc']  # No zqual for these, but they are confirmed galaxies
@@ -143,14 +181,14 @@ def process_halo7d_survey(survey_dir, target_dir, known_zqual):
                 continue  # We don't trust that these serendipitous objects are galaxies
 
             objid = unique_obj_id(maskname, objname)
-            if objid not in known_zqual:
+            if objid not in known_zqual and objid not in do_not_copy_map:
                 known_zqual[objid] = 0  # Galaxy
                 print('Not in zspec.fits: ' + filepath + ' --> ' + target_dir + '/nonstar')
-                shutil.copy(filepath, target_dir + '/nonstar')
+                #shutil.copy(filepath, target_dir + '/nonstar')
 
 
 # Process VDGC survey
-def process_vdgc_survey(survey_dir, target_dir, known_zqual):
+def process_vdgc_survey(survey_dir, target_dir, known_zqual, do_not_copy_map):
     zspec_fits = glob.glob(survey_dir + '/zspec/zspec*.fits')
     for fits_file in zspec_fits:
         zspec = fits.open(fits_file)
@@ -159,15 +197,16 @@ def process_vdgc_survey(survey_dir, target_dir, known_zqual):
     
         print(fits_file)
         # TODO: Should we exclude serendip from VDGC? We do have ZQual for them (most nonstars).
-        copy_from_1_directory_zspec_fits(survey_dir, data, target_dir, known_zqual)
+        copy_from_1_directory_zspec_fits(survey_dir, data, target_dir, known_zqual, do_not_copy_map)
 
 
 # All halo7d files NOT mentioned in zspec.fits or ppxf.dat are nonstars.
 # Use this map to keep track of them.
 known_zqual = {}  # Empty dict
+do_not_copy_map = {}  # Empty dict
 
-process_vdgc_survey(os.path.expanduser("~") + '/ast01/vdgc', TARGET_DIR, known_zqual)
-process_halo7d_survey(os.path.expanduser("~") + '/ast01/halo7d', TARGET_DIR, known_zqual)
+process_vdgc_survey(os.path.expanduser("~") + '/ast01/vdgc', TARGET_DIR, known_zqual, do_not_copy_map)
+process_halo7d_survey(os.path.expanduser("~") + '/ast01/halo7d', TARGET_DIR, known_zqual, do_not_copy_map)
 
 print('\n++++++++ SUMMARY')
 # Invert the map so we can print summary stats
